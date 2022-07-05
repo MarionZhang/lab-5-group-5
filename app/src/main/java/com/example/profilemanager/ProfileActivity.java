5baa259b1c7101537920df6dbc9b940f9bd66c43
Weiwei Zhang <wzhan228@uottawa.ca>
2022-07-04 21:40:51 -0400

    and onCreate

--- app/src/main/java/com/example/profilemanager/ProfileActivity.java
+++ app/src/main/java/com/example/profilemanager/ProfileActivity.java
@@ -1,12 +1,22 @@
 package com.example.profilemanager;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+    }
 
     public void SetTeamIcon(View view){
         Intent returnIntent = new Intent();
@@ -15,5 +25,6 @@ public class ProfileActivity extends AppCompatActivity {
         setResult(RESULT_OK, returnIntent);
         finish();
     }
+
 
 }
